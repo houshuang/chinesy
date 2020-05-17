341b90c70a9acf6b51d4e54b53de4db583692adf
Stian Haklev <stian@minervaproject.com>
2020-05-17 19:11:34 +0200

    WIP

--- src/App.js
+++ src/App.js
@@ -34,7 +34,6 @@ const Character = ({ char, state, setState, shift, meta }) => {
       {hanzi[char][4]} - {hanzi[char][5].replace(/\//g, " /")}
     </Tooltip>
   );
-
   return (
     <div onClick={onClick} className={`character sel-${state}`}>
       {shift || meta ? (
@@ -58,6 +57,7 @@ const App = () => {
   const [shift, setShift] = React.useState();
   const [meta, setMeta] = React.useState();
 
+  const [view, setView] = React.useState("new");
   const onKeyUp = e => {
     setShift(false);
     setMeta(false);
@@ -77,19 +77,29 @@ const App = () => {
           acc[x] = newNum;
           return acc;
         }, charState);
+        setCharState({ ...res });
         localStorage.setItem("characters", JSON.stringify(res));
-        setCharState(res);
         e.preventDefault();
       }
     }
   };
   React.useEffect(() => {
+    if (view === "new") {
+      const base = Object.keys(hanzi)
+        .slice(0, 1000)
+        .filter(x => !charState[x]);
+      setChar(sampleSize(base, 50));
+    } else {
+      setChar(Object.keys(charState));
+    }
+  }, [view]);
+
+  React.useEffect(() => {
     const existingRaw = localStorage.getItem("characters");
-    const existing = JSON.parse(existingRaw);
-    console.log(existing);
+    const existing = JSON.parse(existingRaw) || {};
     setCharState(existing);
     const base = Object.keys(hanzi)
-      .slice(0, 150)
+      .slice(0, 1000)
       .filter(x => !existing[x]);
     setChar(sampleSize(base, 50));
   }, []);
@@ -99,14 +109,73 @@ const App = () => {
     document.onkeyup = onKeyUp;
   });
 
+  const ref = React.useRef();
+  const popup = props => (
+    <Tooltip className="popup" id="button1-tooltip" {...props}>
+      <div>
+        <p>
+          Level 1:{" "}
+          {Object.keys(charState).filter(x => charState[x] === 1).length}
+        </p>
+        <p>
+          Level 2:{" "}
+          {Object.keys(charState).filter(x => charState[x] === 2).length}
+        </p>
+        <p>
+          Level 3:{" "}
+          {Object.keys(charState).filter(x => charState[x] === 3).length}
+        </p>
+      </div>
+    </Tooltip>
+  );
+
+  const toggleView = () => {
+    if (view === "new") {
+      setView("old");
+    } else {
+      setView("new");
+    }
+  };
+
   return (
-    <Characters
-      char={char}
-      charState={charState}
-      setCharState={setCharState}
-      shift={shift}
-      meta={meta}
-    />
+    <div className="App">
+      <h1>Chinesy</h1>
+      <p>Systematically test your knowledge of Chinese characters</p>
+
+      <OverlayTrigger
+        placement="auto"
+        delay={{ show: 250, hide: 400 }}
+        overlay={popup}
+      >
+        <div ref={ref} onClick={toggleView}>
+          {view === "new" ? (
+            <span>
+              {Object.keys(charState).length} characters reviewed (click to see)
+            </span>
+          ) : (
+            <span>Click to review new characters</span>
+          )}
+        </div>
+      </OverlayTrigger>
+      <div
+        style={{ marginTop: "10px" }}
+        onClick={() => {
+          if (window.confirm("Are you sure?")) {
+            localStorage.setItem("characters", "{}");
+            setCharState({});
+          }
+        }}
+      >
+        Clear storage and state
+      </div>
+      <Characters
+        char={char}
+        charState={charState}
+        setCharState={setCharState}
+        shift={shift}
+        meta={meta}
+      />
+    </div>
   );
 };
 
@@ -121,7 +190,6 @@ const Characters = ({ char, charState, setCharState, shift, meta }) => {
           shift={shift}
           meta={meta}
           setState={e => {
-            console.log(e);
             localStorage.setItem(
               "characters",
               JSON.stringify({ ...charState, [x]: e })
