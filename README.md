bb9b7bc042c8517711474c1b2fd695de8591049d
Stian Haklev <stian@minervaproject.com>
2020-05-17 20:30:33 +0200

    WIP

--- README.md
+++ README.md
@@ -1,68 +1,21 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# Chinesy
 
-## Available Scripts
+This is intended as a quick way of testing not only how many characters you know, but also which exact characters. It's built for my own use-case, but feel free to play with it.
 
-In the project directory, you can run:
+[More info about my Chinese learning](https://notes.reganmian.net/a--chinese)
 
-### `yarn start`
+## Data sources
+- Hanzi frequency from [Jun Da's list](https://lingua.mtsu.edu/chinese-computing/statistics/char/list.php?Which=MO)
+- Bigrams from [BLCU Chinese Corpus - global word frequency](https://www.plecoforums.com/threads/word-frequency-list-based-on-a-15-billion-character-corpus-bcc-blcu-chinese-corpus.5859/)
 
-Runs the app in the development mode.<br />
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+## Usage
+The idea is that you would like to establish for example how many of the first 1000 characters that you actually know, and how well you know them.
 
-The page will reload if you make edits.<br />
-You will also see any lint errors in the console.
-
-### `yarn test`
-
-Launches the test runner in the interactive watch mode.<br />
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `yarn build`
-
-Builds the app for production to the `build` folder.<br />
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.<br />
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `yarn eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
-
-### Analyzing the Bundle Size
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `yarn build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+- Open the page, and choose from 0 to 1000, showing 50 characters each time.
+- For each character, click to toggle between three levels of knowledge, or not knowing. 
+  - For me, this corresponds to being completely sure about pronunciation and meaning, being able to read it in context, knowing something about it (guessing meaning without prounciation for example), or not knowing, but you can define it however you see fit
+- To toggle all characters on screen into a certain state, click shift+1/2/3 or shift+0 to reset
+- All selections are stored in localStorage, and will be there the next time you open the browser. No data is sent to any server.
+- You can see how many characters you have already processed, if you hover over that line, you'll see how many in each category, and click on it to toggle between seeing all characters processed, and seeing new characters.
+- If you hold shift and hover over a character, you see the pinyin and translation (I use this to check whether I'm correct, if I am in doubt). If you hold Cmd (on Mac) and hover, you see four bigrams with this character in them (I use this to see if I can recognize the character in context)
+- You can click "Download" to get a CSV file with all the characters you've processed, the global frequency, the translation, etc.
--- src/App.js
+++ src/App.js
@@ -6,6 +6,27 @@ import words from "./words.mjs";
 import { sampleSize } from "lodash";
 import Tooltip from "react-bootstrap/Tooltip";
 import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import FormControl from "react-bootstrap/FormControl";
+import FileSaver from "file-saver";
+
+const download = data => {
+  const text =
+    `character\tknowledgeState\tfrequencyOrder\tpinyin\ttranslation\texampleNgrams\n` +
+    Object.keys(data)
+      .map(char => {
+        const relWords = words
+          .filter(x => x.includes(char))
+          .slice(0, 4)
+          .join(",");
+        return `${char}\t${data[char]}\t${hanzi[char][0]}\t${hanzi[char][4]}\t${hanzi[char][5]}\t${relWords}`;
+      })
+      .join("\n");
+  const blob = new Blob([text], {
+    type: "text/plain;charset=utf-8"
+  });
+  const fname = "chinesy.tsv";
+  FileSaver.saveAs(blob, fname, true);
+};
 
 const Character = ({ char, state, setState, shift, meta }) => {
   const ref = React.useRef(null);
@@ -51,6 +72,10 @@ const Character = ({ char, state, setState, shift, meta }) => {
   );
 };
 const App = () => {
+  const [redraw, setRedraw] = React.useState();
+  const [max, setMax] = React.useState(1000);
+  const [min, setMin] = React.useState(0);
+  const [nchar, setN] = React.useState(50);
   const [char, setChar] = React.useState([]);
   const [charState, setCharState] = React.useState({});
 
@@ -58,6 +83,7 @@ const App = () => {
   const [meta, setMeta] = React.useState();
 
   const [view, setView] = React.useState("new");
+  const [ready, setReady] = React.useState();
   const onKeyUp = e => {
     setShift(false);
     setMeta(false);
@@ -67,41 +93,55 @@ const App = () => {
     if (e) {
       if (e.shiftKey) {
         setShift(true);
+        if (e.which > 47 && e.which < 52) {
+          const newNum = e.which - 48;
+          const res = char.reduce((acc, x) => {
+            acc[x] = newNum;
+            return acc;
+          }, charState);
+          setCharState({ ...res });
+          localStorage.setItem("chinesy.characters", JSON.stringify(res));
+          e.preventDefault();
+        }
       }
       if (e.metaKey) {
         setMeta(true);
-      }
-      if (e.which > 47 && e.which < 52) {
-        const newNum = e.which - 48;
-        const res = char.reduce((acc, x) => {
-          acc[x] = newNum;
-          return acc;
-        }, charState);
-        setCharState({ ...res });
-        localStorage.setItem("characters", JSON.stringify(res));
-        e.preventDefault();
       }
     }
   };
   React.useEffect(() => {
     if (view === "new") {
       const base = Object.keys(hanzi)
-        .slice(0, 1000)
+        .slice(min, max)
         .filter(x => !charState[x]);
-      setChar(sampleSize(base, 50));
+      setChar(sampleSize(base, nchar));
     } else {
-      setChar(Object.keys(charState));
+      setChar(Object.keys(charState).filter(x => charState[x] !== 0));
     }
-  }, [view]);
+  }, [view, max, min, redraw]);
 
   React.useEffect(() => {
-    const existingRaw = localStorage.getItem("characters");
+    const existingMax =
+      parseInt(localStorage.getItem("chinesy.max"), 10) || 1000;
+    const existingMin = parseInt(localStorage.getItem("chinesy.min"), 10) || 0;
+    const existingN = parseInt(localStorage.getItem("chinesy.n"), 10) || 0;
+    if (existingMax !== max) {
+      setMax(existingMax);
+    }
+    if (existingMin !== min) {
+      setMin(existingMin);
+    }
+    if (existingN !== nchar) {
+      setN(existingN);
+    }
+    const existingRaw = localStorage.getItem("chinesy.characters");
     const existing = JSON.parse(existingRaw) || {};
     setCharState(existing);
     const base = Object.keys(hanzi)
       .slice(0, 1000)
       .filter(x => !existing[x]);
     setChar(sampleSize(base, 50));
+    setReady(true);
   }, []);
 
   React.useEffect(() => {
@@ -137,46 +177,124 @@ const App = () => {
     }
   };
 
-  return (
-    <div className="App">
-      <h1>Chinesy</h1>
-      <p>Systematically test your knowledge of Chinese characters</p>
+  const textInput = React.useRef();
+  const textInputMin = React.useRef();
+  const textInputnchar = React.useRef();
 
-      <OverlayTrigger
-        placement="auto"
-        delay={{ show: 250, hide: 400 }}
-        overlay={popup}
-      >
-        <div ref={ref} onClick={toggleView}>
-          {view === "new" ? (
-            <span>
-              {Object.keys(charState).length} characters reviewed (click to see)
-            </span>
-          ) : (
-            <span>Click to review new characters</span>
-          )}
-        </div>
-      </OverlayTrigger>
-      <div
-        style={{ marginTop: "10px" }}
-        onClick={() => {
-          if (window.confirm("Are you sure?")) {
-            localStorage.setItem("characters", "{}");
-            setCharState({});
-          }
-        }}
-      >
-        Clear storage and state
+  const handleChange = () => {
+    let newMax = parseInt(textInput.current.value, 10);
+    let newMin = parseInt(textInputMin.current.value, 10);
+    let newn = parseInt(textInputnchar.current.value, 10);
+    if (newMax < newMin) {
+      newMax = newMin + 1000;
+    }
+    if (newMin > newMax) {
+      newMin = 0;
+    }
+    localStorage.setItem("chinesy.max", newMax);
+    localStorage.setItem("chinesy.min", newMin);
+    localStorage.setItem("chinesy.n", newn);
+    setMax(newMax);
+    setMin(newMin);
+    setN(newn);
+
+    setRedraw(new Date());
+  };
+
+  return ready ? (
+    <>
+      <div style={{ position: "absolute", top: "0px", left: "0px" }}>
+        <a href="https://github.com/houshuang/chinesy">
+          <img
+            width="149"
+            height="149"
+            src="https://github.blog/wp-content/uploads/2008/12/forkme_left_white_ffffff.png?resize=149%2C149"
+            class="attachment-full size-full"
+            alt="Fork me on GitHub"
+            data-recalc-dims="1"
+          />
+        </a>
       </div>
-      <Characters
-        char={char}
-        charState={charState}
-        setCharState={setCharState}
-        shift={shift}
-        meta={meta}
-      />
-    </div>
-  );
+      <div className="App">
+        <h1>Chinesy</h1>
+        <p>Systematically test your knowledge of Chinese characters</p>
+
+        <OverlayTrigger
+          placement="auto"
+          delay={{ show: 250, hide: 400 }}
+          overlay={popup}
+        >
+          <div ref={ref} onClick={toggleView}>
+            {view === "new" ? (
+              <span className="buttonLike">
+                {Object.keys(charState).filter(x => charState[x] !== 0).length}{" "}
+                characters reviewed (click to see)
+              </span>
+            ) : (
+              <span>Click to review new characters</span>
+            )}
+          </div>
+        </OverlayTrigger>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-evenly",
+            marginTop: "30px"
+          }}
+        >
+          <div>
+            <div
+              className="buttonLike"
+              style={{ marginTop: "10px" }}
+              onClick={() => {
+                if (window.confirm("Are you sure?")) {
+                  localStorage.setItem("chinesy.characters", "{}");
+                  setCharState({});
+                }
+              }}
+            >
+              Clear storage and state
+            </div>
+            <div
+              className="buttonLike"
+              style={{ marginTop: "20px" }}
+              onClick={() => download(charState)}
+            >
+              Download data
+            </div>
+          </div>
+          <div className="maxmin">
+            <div>
+              Start with nth most frequent{" "}
+              <FormControl ref={textInputMin} type="text" defaultValue={min} />
+            </div>
+            <div>
+              Until nth most frequent{" "}
+              <FormControl ref={textInput} type="text" defaultValue={max} />
+            </div>
+            <div>
+              Show n characters at a time{" "}
+              <FormControl
+                ref={textInputnchar}
+                type="text"
+                defaultValue={nchar}
+              />
+            </div>
+            <span className="buttonLike" onClick={handleChange}>
+              Shuffle
+            </span>
+          </div>
+        </div>
+        <Characters
+          char={char}
+          charState={charState}
+          setCharState={setCharState}
+          shift={shift}
+          meta={meta}
+        />
+      </div>
+    </>
+  ) : null;
 };
 
 const Characters = ({ char, charState, setCharState, shift, meta }) => {
@@ -191,7 +309,7 @@ const Characters = ({ char, charState, setCharState, shift, meta }) => {
           meta={meta}
           setState={e => {
             localStorage.setItem(
-              "characters",
+              "chinesy.characters",
               JSON.stringify({ ...charState, [x]: e })
             );
             setCharState({ ...charState, [x]: e });
